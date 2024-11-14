0ad07953d6ff859b61b4327b4ed29e1e22cd056e
اِسْمَةُ الله بختيرو <139554680+baxt1y0rov@users.noreply.github.com>
2024-11-14 16:10:23 +0500

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // If you want to externalize @mui/system, uncomment the line below
+      // external: ['@mui/system'],
+    },
+  },
+});
